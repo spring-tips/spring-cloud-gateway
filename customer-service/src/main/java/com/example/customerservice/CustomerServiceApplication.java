8a81ab2fdc73583997bd603e1b6cacf0819dd1c1
Josh Long <starbuxman@gmail.com>
2017-11-27 19:06:02 -0800

    gateway code

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -2,54 +2,47 @@ package com.example.customerservice;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
+    @Bean
+    ApplicationRunner init(CustomerRepository cr) {
+        return args ->
+                cr.deleteAll()
+                        .thenMany(Flux.just("A", "B", "C").map(l -> new Customer(null, l)).flatMap(cr::save))
+                        .thenMany(cr.findAll())
+                        .subscribe(System.out::println);
+    }
+
+    @Bean
+    RouterFunction<?> routes(CustomerRepository cr) {
+        return
+                route(GET("/customers"), r -> ok().body(cr.findAll(), Customer.class))
+                        .andRoute(GET("/customers/{id}"), r -> ok().body(cr.findById(r.pathVariable("id")), Customer.class))
+                        .andRoute(GET("/delay"), r -> ok().body(Flux.just("Hello, world!").delayElements(Duration.ofSeconds(10)), String.class));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
-    @Bean
-    RouterFunction<?> routes(CustomerRepository repo) {
-        return RouterFunctions
-                .route(GET("/customers"), r -> ok().body(repo.findAll(), Customer.class))
-                .andRoute(GET("/delay"), r -> ok().body(
-                        Flux.<String>generate(s -> s.next("Hello, world")).take(2).delayElements(Duration.ofSeconds(20)), String.class))
-                .andRoute(GET("/customers/{id}"), r -> ok().body(repo.findById(r.pathVariable("id")), Customer.class));
-    }
-
-    private static Mono<ServerResponse> log(ServerRequest r, Mono<ServerResponse> responseMono) {
-
-        return responseMono;
-    }
-
-    @Bean
-    ApplicationRunner init(CustomerRepository repository) {
-        return a -> repository
-                .deleteAll()
-                .thenMany(Flux.just("A", "B", "C").flatMap(x -> repository.save(new Customer(null, x))))
-                .thenMany(repository.findAll())
-                .subscribe(System.out::println);
-    }
 }
+
 
 interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
 }
@@ -57,7 +50,10 @@ interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
 @Document
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 class Customer {
+
+    @Id
     private String id;
-    private String email;
+    private String name;
 }
--- edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
@@ -17,9 +17,7 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -28,96 +26,87 @@ import reactor.core.publisher.Mono;
 
 import java.util.stream.IntStream;
 
-import static org.springframework.cloud.gateway.filter.factory.GatewayFilters.hystrix;
-import static org.springframework.cloud.gateway.filter.factory.GatewayFilters.rewritePath;
-import static org.springframework.cloud.gateway.handler.predicate.RoutePredicates.host;
 import static org.springframework.cloud.gateway.handler.predicate.RoutePredicates.path;
 
-
-/**
- * @todo demo this endpoint http://localhost:8081/application/gateway/routes
- */
-@SpringBootApplication
 @EnableWebFluxSecurity
+@SpringBootApplication
 public class EdgeServiceApplication {
 
-    // this works by itself.
     @Bean
-    DiscoveryClientRouteDefinitionLocator routes(DiscoveryClient dc) {
+    ApplicationRunner client() {
+        return args -> {
+            WebClient client = WebClient.builder().filter(ExchangeFilterFunctions.basicAuthentication("user", "pw")).build();
+            Flux.fromStream(IntStream.range(0, 100).boxed())
+                    .flatMap(number -> client.get().uri("http://localhost:8081/rl").exchange())
+                    .flatMap(clientResponse -> clientResponse.toEntity(String.class).map(re -> String.format("status: %s; body: %s", re.getStatusCodeValue(), re.getBody())))
+                    .subscribe(System.out::println);
+        };
+    }
+
+    @Bean
+    MapReactiveUserDetailsService authentication() {
+        return new MapReactiveUserDetailsService(
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("pw")
+                        .roles("USER")
+                        .build());
+    }
+
+    @Bean
+    SecurityWebFilterChain authorization(ServerHttpSecurity security) {
+        return security
+                .authorizeExchange().pathMatchers("/rl").authenticated()
+                .anyExchange().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator discoveryRoutes(DiscoveryClient dc) {
         return new DiscoveryClientRouteDefinitionLocator(dc);
     }
 
     @Bean
-    ReactiveUserDetailsService userDetailsRepository() {
-        UserDetails user = User
-                .withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new MapReactiveUserDetailsService(user);
-    }
-
-    @Bean
-    ApplicationRunner rl() {
-        WebClient client = WebClient.create().mutate().filter(ExchangeFilterFunctions.basicAuthentication("user", "password")).build();
-        return args -> Flux
-                .fromStream(IntStream.range(0, 100).boxed())
-                .flatMap(i -> client.get().uri("http://localhost:8081/rl").exchange())
-                .flatMap(cr -> cr.toEntity(String.class).map(re -> String.format("status: %s; body: %s", re.getStatusCode().value(), re.getBody())))
-                .subscribe(System.out::println);
-    }
-
-    @Bean
-    RouteLocator gwRoutes(RequestRateLimiterGatewayFilterFactory rateLimiter) {
-        String loadBalancedCustomerService = "lb://customer-service/customers";
+    RouteLocator gatewayRoutes(RequestRateLimiterGatewayFilterFactory rl) {
         return Routes
                 .locator()
                 // basic proxy
                 .route("start")
                 .predicate(path("/start"))
                 .uri("http://start.spring.io:80/")
-                // load-balanced requests
-                .route("cs")
-                .predicate(path("/cs"))
-                .uri(loadBalancedCustomerService)
-                // rate limiter
-                .route("rl")
-                .predicate(path("/rl"))
-                .filter(rateLimiter.apply(RedisRateLimiter.args(5, 10)))
-                .uri(loadBalancedCustomerService)
+                // load balanced proxy
+                .route("lb")
+                .predicate(path("/lb"))
+                .uri("lb://customer-service/customers")
+                // custom filter 1
+                .route("cf1")
+                .predicate(path("/cf1"))
+                .filter((exchange, chain) ->
+                        chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                            ServerHttpResponse httpResponse = exchange.getResponse();
+                            httpResponse.setStatusCode(HttpStatus.CONFLICT);
+                            httpResponse.getHeaders().setContentType(MediaType.APPLICATION_PDF);
+                        }))
+                )
+                .uri("lb://customer-service/customers")
+                // custom filter 2
+                .route("cf2")
+                .predicate(path("/cf2/**"))
+                .filter(GatewayFilters.rewritePath("/cf2/(?<CID>.*)", "/customers/${CID}"))
+                .uri("lb://customer-service")
                 // circuit breaker
                 .route("cb")
                 .predicate(path("/cb"))
-                .filter(hystrix("cb"))
+                .filter(GatewayFilters.hystrix("cb"))
                 .uri("lb://customer-service/delay")
-                // custom filter #1
-                .route("custom-filter-1")
-                .predicate(path("/cf1"))
-                .filter((exchange, filter) -> filter
-                        .filter(exchange)
-                        .then(Mono.fromRunnable(() -> {
-                            ServerHttpResponse response = exchange.getResponse();
-                            response.getHeaders().setContentType(MediaType.APPLICATION_OCTET_STREAM);
-                            response.setStatusCode(HttpStatus.CONFLICT);
-                        })))
-                .uri(loadBalancedCustomerService)
-                // custom filter #2: curl -v http://localhost:8081/cf2/5a1bfecdd95bb0425fbeaf4d
-                .route("custom-filter-2")
-                .predicate(path ("/cf2/**"))
-                .filter(rewritePath("/cf2/(?<CID>.*)", "/customers/${CID}"))
-                .uri("lb://customer-service")
-                .build();
-    }
-
-    @Bean
-    SecurityWebFilterChain security(ServerHttpSecurity http) throws Exception {
-        return http
-                .httpBasic()
-                .and()
-                .authorizeExchange().pathMatchers("/rl**", "/anything/**").authenticated()
-                .anyExchange().permitAll()
-                .and()
+                // rate limiter
+                .route("rl")
+                .predicate(path("/rl"))
+                .filter(rl.apply(RedisRateLimiter.args(5, 10)))
+                .uri("lb://customer-service/customers")
                 .build();
     }
 
